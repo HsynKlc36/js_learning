e10fb83de89dbe0ed8d8af5da588816b12190e83
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-07-05 09:33:26 +0300

    04 eklendi

---
+++ 04-boolean_veri_turu.js
@@ -0,0 +1,42 @@
+//*******Boolean Veri Turu ile Calısmak*********
+
+//0 ve 1'i Anlamak
+let isActive = false;
+isActive = true;
+console.log(isActive);
+
+let userName;
+let isUserName = Boolean(userName);
+console.log(isUserName);
+
+Boolean("11");//true
+Boolean("0"); //true
+Boolean("");//false
+//içinde bilgi varsa true yoksa false
+userName = "user";
+
+console.log("user Name: ", Boolean(userName));
+
+//0,-0,null,false,Nan,undefined,("")->false döner;
+Boolean(0); // false
+Boolean(-0); // false
+Boolean(-0.1);//true
+Boolean(0 === 0);//true
+Boolean(userName.length > 0);//true
+const b5 = Boolean('false');//true
+const c4 = Boolean(false);//false
+const c1 = Boolean(undefined);//false
+const c2 = Boolean(null);//false
+const c5 = Boolean(NaN);//false
+
+//*** TRUE GELEN DEĞERLER****
+Boolean({}) // true
+Boolean([]) // true
+Boolean(Symbol()) // true
+!!Symbol() // true
+Boolean(function () { }) // true
+
+console.log(true + false)
+//"+" operatörü numeric işlem yapar bu durumlardan boolean ifadeler numaric coercion'a uğrar.
+//Number(true) + Number(false) dönüşen ifade
+//1 + 0 şeklini alır Sonuç = 1
