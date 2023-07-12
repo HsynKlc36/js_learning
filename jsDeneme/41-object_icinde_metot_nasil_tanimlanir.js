17a9a98a371aecbda5d5db202827ad9fbca64b1a
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-07-12 16:08:48 +0300

    42 tamamlandı!

--- jsDeneme/41-object_icinde_metot_nasil_tanimlanir.js
+++ jsDeneme/41-object_icinde_metot_nasil_tanimlanir.js
@@ -16,6 +16,7 @@ let user1 = {
 console.log(user1)
 console.log(user1.shortName())
 console.log(user1.shortName2())
+console.log(user1.hasOwnProperty('lastName')) //user1 içerisinde böyle bir prop varsa true yoksa false döner!
 
 //Metot Nedir ?
 //    JavaScript'in referans veri tiplerinden biri olan ve Türkçede "nesne" anlamına gelen object key-value (anahtar-değer) yapı mantığına sahip bir veri tipidir. Yani bir nesne oluştururken bu nesnemiz için bir veya birden fazla property (özellik) tanımlanabilir ve key-value mantığındaki gibi nesnemizin sahip olduğu özellikler için değerler verilmelidir. Özelliklerin sahip olduğu değerler farklı veri tiplerinde olabileceği gibi bir fonksiyonu da işaret edebilir. Bir özellik eğer bir fonksiyonu işaret ediyorsa bu özelliğe metot denir.
---
+++ jsDeneme/42_spread_rest_destructure.js
@@ -0,0 +1,66 @@
+const studentObjList = [
+    { name: "Halil Can Toptas", course: "Frontend Dev", instructor: "Orkun Durmaz", score: 80 },
+    { name: "Serkan Duman", course: "Frontend Dev", instructor: "Orkun Durmaz", score: 70 },
+    { name: "Aysenur Altinsoy", course: "Frontend Dev", instructor: "Orkun Durmaz", score: 90 },
+    { name: "Arkin Zat", course: "Frontend Dev", instructor: "Orkun Durmaz", score: 65 },
+];
+
+const student1 = { name: "Halil Can Toptas", course: "Frontend Dev", instructor: "Orkun Durmaz", score: 80 };
+
+const studentNames = [
+    "Mehmet İkbal Dündar",
+    "Arkın Zat",
+    "Ayşenur Altınsoy",
+    "Derin Perinçek",
+    "Enes Cihan"
+];
+
+
+
+//spread (ES6)
+console.log(studentNames[0], studentNames[1], studentNames[2], studentNames[3], studentNames[4]);
+console.log(...studentNames);//Mehmet İkbal Dündar Arkın Zat Ayşenur Altınsoy Derin Perinçek Enes Cihan
+console.log([...studentNames]);//[ "Mehmet İkbal Dündar","Arkın Zat","Ayşenur Altınsoy","Derin Perinçek","Enes Cihan",]
+
+const newStudentNames = ["Hazel Calkar", ...studentNames, "Serkan Temiz"];
+console.log(newStudentNames);//["Hazel Calkar", "Mehmet İkbal Dündar","Arkın Zat","Ayşenur Altınsoy","Derin Perinçek","Enes Cihan","Serkan Temiz"]
+
+const student2 = { ...student1, name: "Osman Bursa", age: 27 };
+console.log(student2);// student2 = { name: "Osman Bursa", course: "Frontend Dev", instructor: "Orkun Durmaz", score: 80,age:27 };
+
+//rest
+const greetAll = (param1, ...people) => {
+    console.log(people)//['Arkın','Orkun']
+    console.log(param1)//Ikbal
+}
+
+greetAll("Ikbal", "Arkin", "Orkun");
+
+const greetArray = ([param1, ...param2]) => {
+    console.log(param2)//["Arkın Zat","Ayşenur Altınsoy","Derin Perinçek","Enes Cihan"]
+    console.log(param1)//Mehmet İkbal Dündar
+}
+greetArray(studentNames);
+
+//Not:aşağıda rest uygulamadık! fakat dizinin ilk iki parametresini yakaladık! kalan kısmı rest ile alabilirdik fakat kullanmayacağımız için almadık!
+const greetArray2 = ([param1, param2]) => {
+    console.log(param1)//Mehmet İkbal Dündar
+    console.log(param2)//Arkın Zat
+}
+greetArray2(studentNames);
+
+
+//destructure
+// const studentName1 = studentNames[0];
+// const studentName2 = studentNames[1];
+// const studentName3 = studentNames[2];
+
+// const studentProp1 = student.name;
+// const studentProp2 = student.course;
+// const studentProp3 = student.name;
+
+const [studentName1, studentName2, studentName3, ...restOfStudents] = studentNames;
+console.log(studentName2);//Arkın Zat
+const { name, course, ...instructor } = student1;// nesne içerisinde destructure yapılan öğeler 1 den fazla ise nesne şeklinde fakat 1 tane ise o keye karşılık gelen value'yu doğrudan getirir.İsim olarak propert'ler eşleşmelidir yani name yazmazsak student1 içerisindeki name propert'siyle eşleşmeyecektir! .Sadece ...instructor yerine farklı isimlendirme yapılabilir.Çünkü içerisinde birden fazla prop olacağı için doğrudan bir property'nin adıyla eşleşmeyecek ve bu yüzden doğrudan prop adı  taşımayacağından farklı isimle tanımlanabilir.mesela aşağıda görüldüğü gibi hem instructor hemde score'u tutmaktadır yani ...abc olarak bile tanımlayabilirim !
+console.log(instructor);//{instructor:'Orkun Durmaz',score:80}
+console.log(course);//Frontend Dev
